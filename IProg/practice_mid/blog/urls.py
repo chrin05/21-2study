a9c9333caee66687c42dfc1fdd988c7b02ec9ded
chrin05 <rinring105@gmail.com>
2021-10-17 17:46:45 +0900

    웹페이지 만들기(8장)

---
+++ IProg/practice_mid/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    #fbv방식
+    path('',views.index),
+    path('<int:pk>/', views.single_post_page),
+    #cbv방식
+    #path('',views.PostList.as_view()),
+    #path('<int:pk/', views.PostDetail.as_view()),
+]
--- IProg/practice_mid/blog/views.py
+++ IProg/practice_mid/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Post
+#CBV일때
+# from django.views.generic import ListView,DetailView
 
 # Create your views here.
+#fbv방식
+def index(request) :
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(request, 'blog/index.html',
+                  {'posts' : posts},
+                  )
+
+def single_post_page(request,pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(request, 'blog/single_post_page.html',
+                  {'post' : post},
+                  )
+
+#CBV방식
+#class PostList(ListView):
+#    model = Post
+#    ordering = '-pk'
+#    1) template_name = 'blog/index.html'
+
+#class PostDetail(DetailView):
+#        model = Post
+#        2) template_name = 'blog/single_post_page.html'
